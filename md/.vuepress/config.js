45f306ff002f0021044826e722bbf6d5999fd0f9
yangpeng <aitexiaoy@163.com>
2019-09-03 15:58:37 +0800

    new file

---
+++ md/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: 'airbnb开发规范',
+    description: '收集整理的airbnd中文规范',
+    serviceWorker: false,
+    dest:'../../docs',
+    themeConfig: {
+        search: false,
+        sidebar: [
+            '../es6.md',
+            '../css.md',
+            '../react.md',
+            '../es5.md'
+            // {
+            //     title: 'JavaScript ES6',
+            //     children: ['../es6.md']
+            // },
+            // {
+            //     title: 'CSS 和 Sass',
+            //     collapsable: false,
+            //     children: [
+            //         '../css.md'
+            //     ]
+            // },
+            // {
+            //     title: 'React/JSX',
+            //     children: ['../react.md']
+            // },
+            // {
+            //     title: 'JavaScript ES5',
+            //     children: ['../es5.md']
+            // },
+        ]
+    }
+}
